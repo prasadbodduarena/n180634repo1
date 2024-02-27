a4157d86a61c52cb7001237bbbe575a31737afcc
prasad <prasad@192.168.1.65>
2024-02-27 14:29:49 +0530

    Dev-written -LogicalOp ()

--- A1/src/main/java/com/nit/service/App.java
+++ A1/src/main/java/com/nit/service/App.java
@@ -5,12 +5,13 @@ package com.nit.service;
  *
  */
 public class App 
-{
+{   static  App app=new App();
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(new App().WishMessage("Love ","My india"));
-        System.out.println(new App().Arithmetic(34, 433, false));
+        System.out.println(app.WishMessage("Love ","My india"));
+        System.out.println(app.Arithmetic(34, 433, false));
+        System.out.println(app.LogicalOp(43, 343) );
     }
     public String WishMessage(String msg,String word) {
     	String send=new String(msg);
@@ -22,8 +23,12 @@ public class App
     	if(t>=122) 
     	 return z;
     	else
-    	return z;
-    	
-    	
+    	return z;   	
     }
+   public int LogicalOp(int x,int y) {
+	   if(x!=0)
+	return x;
+	   else
+		   return y;
+   }
 }
