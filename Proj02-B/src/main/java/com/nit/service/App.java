dce5c80493676949192e924b3a30eae787d58e47
prasad <prasad@192.168.1.65>
2024-02-27 12:47:22 +0530

    DEV-modified and written methods

--- Proj02-B/src/main/java/com/nit/service/App.java
+++ Proj02-B/src/main/java/com/nit/service/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+       System.out.println( new App().Arithmetic(12, 32));
+               
+    }
+    public int Arithmetic(int x,int y) {
+    	return x+y;
     }
 }
