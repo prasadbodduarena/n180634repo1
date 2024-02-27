9aa3ac689b7c8f9786dd64680a3d44255244ba70
prasad <prasad@192.168.1.65>
2024-02-27 14:23:43 +0530

    TL-written Arithmetic method

--- A1/src/main/java/com/nit/service/App.java
+++ A1/src/main/java/com/nit/service/App.java
@@ -9,11 +9,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(new App().WishMessage("Love ","My india"));;
+        System.out.println(new App().WishMessage("Love ","My india"));
+        System.out.println(new App().Arithmetic(34, 433, false));
     }
     public String WishMessage(String msg,String word) {
     	String send=new String(msg);
     	String pin=send.concat(word);
     	return pin;
+    }
+   boolean Arithmetic(int x,int y,boolean z){
+    	int t=x+y;
+    	if(t>=122) 
+    	 return z;
+    	else
+    	return z;
+    	
+    	
     }
 }
