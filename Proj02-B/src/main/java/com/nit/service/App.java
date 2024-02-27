13ff3dddd3c76ee38f6d11bd5ca62dbd51c23384
prasad <prasad@192.168.1.65>
2024-02-27 12:51:36 +0530

    TL-written Message methods

--- Proj02-B/src/main/java/com/nit/service/App.java
+++ Proj02-B/src/main/java/com/nit/service/App.java
@@ -10,9 +10,13 @@ public class App
     {
         System.out.println( "Hello World!" );
        System.out.println( new App().Arithmetic(12, 32));
+       System.out.println(new App().newMessage("I love GitHUB"));
                
     }
     public int Arithmetic(int x,int y) {
     	return x+y;
+    }
+    public String newMessage(String msg) {
+    	return msg;
     }
 }
