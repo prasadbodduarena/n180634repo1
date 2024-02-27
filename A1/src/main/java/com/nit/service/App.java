be5ff169128c04d64583b504cd80622e4c99a7f6
prasad <prasad@192.168.1.65>
2024-02-27 14:15:12 +0530

    Dev-written wish method

--- A1/src/main/java/com/nit/service/App.java
+++ A1/src/main/java/com/nit/service/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(new App().WishMessage("Love ","My india"));;
+    }
+    public String WishMessage(String msg,String word) {
+    	String send=new String(msg);
+    	String pin=send.concat(word);
+    	return pin;
     }
 }
